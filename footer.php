38478f10f0cd158cbff33d9720d854127b658227
abdulrahmanfadhl <abdulrahmanfadhl@gmail.com>
2026-02-20 04:38:06 +0300

    feat: Implement new footer styles, and refactor footer content and layout in `footer.php`.

--- footer.php
+++ footer.php
@@ -1,50 +1,55 @@
 <footer class="go_footer">
-    <div class="row">
+    <div class="footer-container">
+        <div class="footer-row">
+            <div class="fot_column brand-col">
+                <h4>GO HOME CLINIC</h4>
+                <p>A clinic that provides essential primary healthcare for elderly people, bringing medical expertise
+                    directly to their doorstep.</p>
+                <div class="social-icons">
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+            </div>
 
-        <div class="fot_column">
-            <h4>GO HOME CLINIC</h4>
-            <p>A clinic that's provided with the necessary
-                equipments for the primary healthcare for
-                elderly people by reaching them to their
-                doorstep.
-            </p>
+            <div class="link_column">
+                <h4>Quick Links</h4>
+                <ul class="link_cool">
+                    <li><a href="Index.php"><i class="fa fa-angle-right"></i> Home</a></li>
+                    <li><a href="faq.php"><i class="fa fa-angle-right"></i> FAQ</a></li>
+                    <li><a href="About.php"><i class="fa fa-angle-right"></i> About Us</a></li>
+                    <li><a href="BookAbo.php"><i class="fa fa-angle-right"></i> Book Appointment</a></li>
+                </ul>
+            </div>
+
+            <div class="cont_column">
+                <h4>Contact Us</h4>
+                <ul class="con_us">
+                    <li>
+                        <i class="fa-solid fa-envelope"></i>
+                        <span>go_home@gmail.com</span>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-phone"></i>
+                        <span>+966 000 000 000</span>
+                    </li>
+                    <li>
+                        <i class="fa-solid fa-location-dot"></i>
+                        <span>Riyadh, Saudi Arabia</span>
+                    </li>
+                    <li>
+                        <!-- <i class="fa-solid fa-clock"></i> -->
+                        <i class="fa-solid fa-clock-rotate-left"></i>
+                        <span>Available 24/7</span>
+                    </li>
+                </ul>
+            </div>
         </div>
-
-        <div class="link_column">
-            <h4>Quick Links</h4>
-            <ul class="link_cool">
-                <li>
-                    <a href="Index.php">
-                        <i class="fa fa-angle-right"></i>
-                        Home</a>
-                </li>
-                <li>
-                <li>
-                    <a href="faq.php">
-                        <i class="fa fa-angle-right"></i>
-                        FAQ</a>
-                </li>
-                <li>
-                    <a href="about.php">
-                        <i class="fa fa-angle-right"></i>
-                        About Us</a>
-                </li>
-            </ul>
-        </div>
-
-        <div class="cont_column">
-            <h4>Contact Us</h4>
-            <ul class="con_us">
-                <li>
-                    <label>Email: <p>go_home@gmail.com</p></label>
-                </li>
-                <li>
-                    <label>Number: <p>+966 000 000 000</p></label>
-                </li>
-
-            </ul>
-        </div>
-
     </div>
-    <p class="copyright">© 2023 All Rights Reserved to GO HOME CLINIC</p>
+    <div class="copyright">
+        <p>© 2026 All Rights Reserved to GO HOME CLINIC</p>
+        <p class="developer">Developed By: <a href="mailto:abdulrahmanfadhl@gmail.com">Abdulrahman Fadhl Ameer Saif</a>
+            | EngAboodSDev@</p>
+    </div>
 </footer>
